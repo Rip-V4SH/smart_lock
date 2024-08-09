f53d984b336d5451c42e19696c78db80757815a8
Igor Almeida <tioban.alm@gmail.com>
2024-08-09 12:58:38 +0000

    criacao de paginas terminada

---
+++ src/pages/Agradecimentos.jsx
@@ -0,0 +1,15 @@
+export default function Agradecimentos() {
+  return (
+    <section>
+        <div>
+            <img src="" alt="" />
+            <h1>Marcio</h1>
+        </div>
+
+        <div>
+            <img src="" alt="" />
+            <h1>Plínio</h1>
+        </div>
+    </section>
+  )
+}
